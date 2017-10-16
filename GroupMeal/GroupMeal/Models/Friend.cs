070c65aa4884f2ae0a800323fc4e916d108403f9
njordan <njordan@KELCE-ACIS22-P>
2017-10-16 18:10:44 -0500

    I updated the FriendPage.

--- GroupMeal/GroupMeal/Models/Friend.cs
+++ GroupMeal/GroupMeal/Models/Friend.cs
@@ -2,6 +2,7 @@
 {
     public class Friend
     {
+        public string image { get; set; }
         public string name { get; set; }
         public string allergies { get; set; }
     }
--- GroupMeal/GroupMeal/Pages/FriendPage.xaml.cs
+++ GroupMeal/GroupMeal/Pages/FriendPage.xaml.cs
@@ -22,11 +22,13 @@ namespace GroupMeal.Pages
             this.FriendsList = new List<Friend>();
 
             Friend friend1 = new Friend();
+            friend1.image = "friends.jpg";
             friend1.name = "Nathan Jordan";
             friend1.allergies = "apples";
             FriendsList.Add(friend1);
 
             Friend friend2 = new Friend();
+            friend2.image = "friends.jpg";
             friend2.name = "Andrew Jordan";
             friend2.allergies = "bananas";
             FriendsList.Add(friend2);
