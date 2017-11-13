5a6c5d10a973053b98aa86ae5d3caef0063c9ce9
0758692 <0758692@KELCE-ACIS20-P>
2017-11-13 17:34:03 -0600

    Added JSons for all classes

---
+++ GroupMeal/GroupMeal/Models/settings.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using Plugin.Settings;
+using Plugin.Settings.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupMeal.Models
+{
+    class settings
+    {
+        private static ISettings AppSettings
+        {
+            get
+            {
+                return CrossSettings.Current;
+            }
+        }
+
+        private const string UserJson = "";
+        private static readonly string UserJsonDefault = "";
+
+        private const string CategoryJson = "";
+        private static readonly string CategoryJsonDefault = "";
+
+        private const string FriendJson = "";
+        private static readonly string FriendJsonDefault = "";
+
+        private const string OccasionJson = "";
+        private static readonly string OccasionDefault = "";
+
+        private const string RecipeJson = "";
+        private static readonly string RecipeDefault = "";
+
+        public static category categoryData
+        {
+            get
+            {
+                string data = AppSettings.GetValueOrDefault(CategoryJson, CategoryJsonDefault);
+                if (String.IsNullOrEmpty(data))
+                {
+                    return null;
+                }
+                else
+                {
+                    return JsonConvert.DeserializeObject<category>(data);
+                }
+            }
+            set
+            {
+                string data = JsonConvert.SerializeObject(value);
+                AppSettings.AddOrUpdateValue(CategoryJson, data);
+            }
+        }
+
+       
+    }
+}
